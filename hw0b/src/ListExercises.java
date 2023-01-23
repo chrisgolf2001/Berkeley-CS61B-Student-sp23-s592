3737122a035c88bd1f5f1c0f16076b21adad4801
Matt <i.w.b.s.bilm@gmail.com>
2023-01-22 17:48:12 -0800

    hw0b completed

--- hw0b/src/ListExercises.java
+++ hw0b/src/ListExercises.java
@@ -1,25 +1,57 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListExercises {
 
     /** Returns the total sum in a list of integers */
 	public static int sum(List<Integer> L) {
-        return 0;
+
+        int sum = 0;
+        for(int x = 0; x < L.size(); x++){
+            sum+=L.get(x);
+        }
+        return sum;
     }
 
     /** Returns a list containing the even numbers of the given list */
     public static List<Integer> evens(List<Integer> L) {
-        return null;
+        List <Integer> evenList = new ArrayList<>();
+
+        for(int x = 0; x < L.size();x++){
+            if(L.get(x) % 2 == 0){
+                evenList.add(L.get(x));
+            }
+        }
+        return evenList;
     }
 
     /** Returns a list containing the common item of the two given lists */
     public static List<Integer> common(List<Integer> L1, List<Integer> L2) {
-        return null;
+        List<Integer> commonList = new ArrayList<>();
+        for(int x = 0; x < L1.size(); x++){
+            for(int i = 0; i <L2.size();i++){
+                if(L1.get(x) == L2.get(i)){
+                    commonList.add(L1.get(x));
+                }
+            }
+        }
+
+        return commonList;
     }
 
 
     /** Returns the number of occurrences of the given character in a list of strings. */
     public static int countOccurrencesOfC(List<String> words, char c) {
-        return 0;
+        int occured = 0;
+        for(int x = 0; x < words.size(); x++){
+            for(int i = 0; i < words.get(x).length(); i++){
+                if(c == words.get(x).charAt(i)){
+                    occured++;
+                }
+            }
+        }
+        return occured;
     }
 }
+
+
--- hw0b/src/MapExercises.java
+++ hw0b/src/MapExercises.java
@@ -1,23 +1,56 @@
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class MapExercises {
     /** Returns a map from every lower case letter to the number corresponding to that letter, where 'a' is
      * 1, 'b' is 2, 'c' is 3, ..., 'z' is 26.
      */
     public static Map<Character, Integer> letterToNum() {
-        return null;
+       char[] lowAlpha = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        Map<Character,Integer> newMap = new HashMap<>();
+
+        for(int x = 0; x < 26; x++){
+            newMap.put(lowAlpha[x], x+1);
+        }
+
+        return newMap;
     }
 
     /** Returns a map from the integers in the list to their squares. For example, if the input list
      *  is [1, 3, 6, 7], the returned map goes from 1 to 1, 3 to 9, 6 to 36, and 7 to 49.
      */
     public static Map<Integer, Integer> squares(List<Integer> nums) {
-        return null;
+        TreeMap<Integer, Integer> squaredMap = new TreeMap<>();
+
+        if(nums.size() == 0){
+            return squaredMap;
+        }else {
+            for (int x = 0; x < nums.size(); x++) {
+                squaredMap.put(nums.get(x), nums.get(x) * nums.get(x));
+            }
+        }
+        return squaredMap;
     }
 
     /** Returns a map of the counts of all words that appear in a list of words. */
     public static Map<String, Integer> countWords(List<String> words) {
-        return null;
+
+        int occured = 0;
+        TreeMap<String, Integer> countMap = new TreeMap<>();
+
+        for(int x = 0; x < words.size(); x++){
+            for(int i = 0; i < words.size(); i++){
+                if(words.get(x) == words.get(i)){
+                    occured++;
+                }
+            }
+            if(!countMap.containsKey(words.get(x))){
+                countMap.put(words.get(x),occured);
+            }
+            occured = 0;
+
+
+        }
+
+        return countMap;
     }
 }
