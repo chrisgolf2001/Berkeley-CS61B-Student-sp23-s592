db56e1f53af565c190b80fd5d0ed06ee5b53bba6
Matt <i.w.b.s.bilm@gmail.com>
2023-01-28 15:29:47 -0800

    RandomChooser method completed

--- proj0/src/aoa/choosers/RandomChooser.java
+++ proj0/src/aoa/choosers/RandomChooser.java
@@ -3,30 +3,72 @@ package aoa.choosers;
 import edu.princeton.cs.algs4.StdRandom;
 import aoa.utils.FileUtils;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
 public class RandomChooser implements Chooser {
     private final String chosenWord;
     private String pattern;
 
     public RandomChooser(int wordLength, String dictionaryFile) {
         // TODO: Fill in/change this constructor.
-        chosenWord = "";
+
+          if(wordLength < 1){
+            throw new IllegalArgumentException("Length of the word should be more than 1");
+          }
+
+          List<String> wordsList = FileUtils.readWordsOfLength(dictionaryFile, wordLength);
+
+         if(wordsList.size() == 0){
+            throw new IllegalStateException("There should be at least one word");
+         }
+
+          Collections.sort(wordsList);
+          int randomlyChosenWordNumber = StdRandom.uniform(0, wordsList.size());
+
+//        System.out.println(words);
+//        System.out.println(dictionaryFile);
+//        System.out.println(wordLength);
+          String randomW = wordsList.get(randomlyChosenWordNumber);
+
+
+        chosenWord = randomW;
+        pattern = "-".repeat(chosenWord.length());
+
+        System.out.println(this.chosenWord);
+        System.out.println(this.pattern);
     }
 
     @Override
     public int makeGuess(char letter) {
+        System.out.println(chosenWord);
         // TODO: Fill in this method.
-        return 0;
+        int count = 0;
+        String updatedPattern = "";
+        for(int x = 0; x < chosenWord.length();x++){
+            if(chosenWord.charAt(x) == letter){
+                count++;
+                updatedPattern+=chosenWord.charAt(x);
+            }else {
+                updatedPattern += getPattern().charAt(x);
+            }
+        }
+        this.pattern = updatedPattern;
+        System.out.println(this.pattern);
+        return count;
     }
 
     @Override
     public String getPattern() {
         // TODO: Fill in this method.
-        return "";
+        return pattern;
     }
 
     @Override
     public String getWord() {
         // TODO: Fill in this method.
-        return "";
+        return chosenWord;
     }
 }
