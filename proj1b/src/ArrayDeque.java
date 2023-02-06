dacb066132bf22521e729c8520709ebec9949496
Matt <i.w.b.s.bilm@gmail.com>
2023-02-06 14:44:13 -0800

    initial

---
+++ proj1b/src/ArrayDeque.java
@@ -0,0 +1,155 @@
+import org.checkerframework.checker.units.qual.A;
+import org.checkerframework.checker.units.qual.Length;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.Math.abs;
+import static java.lang.Math.round;
+
+public class ArrayDeque<T> implements Deque<T> {
+
+
+    private int size;
+    private int nextFirst;
+    private int nextLast;
+    private T[] list = (T[]) new Object[8];
+    public static void main(String[] args) {
+        Deque<Character> ad = new ArrayDeque<>();
+        ad.addFirst('a');
+        ad.addFirst('b');
+        ad.addFirst('c');
+        ad.addFirst('d');
+        ad.addFirst('e');
+        ad.addFirst('f');
+        ad.addLast('g');
+        ad.addLast('h');
+        ad.removeFirst ();
+        ad.removeLast();
+        System.out.println(ad.toList());
+        ad.addLast('t');
+
+        ad.addFirst('A');
+        ad.addFirst('B');
+        ad.addFirst('C');
+        ad.addFirst('D');
+        ad.addFirst('E');
+        ad.addFirst('F');
+        ad.addLast('G');
+        ad.addLast('H');
+
+        System.out.println (ad.toList());
+
+    }
+
+    public ArrayDeque() {
+        size = 0;
+        nextFirst = list.length - 1;
+        nextLast = 0;
+    }
+
+    public void resize(int cap) {
+
+        T[] newL = (T[]) new Object[cap];
+
+        System.arraycopy(list,nextLast ,newL,0,size - nextLast);
+        System.arraycopy(list,0 ,newL,size - nextFirst - 1,nextLast);
+        list = newL;
+        nextFirst = list.length - 1;
+        nextLast = size;
+
+    }
+
+
+    @Override
+    public void addFirst(T x) {
+        if (size == list.length) {
+            resize(Integer.valueOf((int) round(size*1.20)));
+        }
+
+        size++;
+        list[nextFirst] = x;
+        nextFirst--;
+
+        }
+
+    @Override
+    public void addLast(T x){
+        if (size == list.length) {
+            resize(size*2);
+        }
+
+        size++;
+        list[nextLast] = x;
+        nextLast++;
+        }
+    @Override
+    public List<T> toList() {
+        List<T> nList = new ArrayList<>();
+        if (nextFirst <= list.length - 1) {
+            for (int x = nextFirst - 1; x != list.length ; x++) {
+                if (list[x] != null) {
+                    nList.add (list[x]);
+                }
+            }
+        }
+
+        if(nextLast != 0) {
+            for (int x = 0; x < nextFirst; x++) {
+                if (list[x] != null) {
+                    nList.add (list[x]);
+                }
+            }
+        }
+
+        return nList;
+    }
+
+    @Override
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    @Override
+    public int size(){
+        return size;
+    }
+
+    @Override
+    public T removeFirst() {
+        size--;
+
+        if (nextFirst == list.length -1) {
+            return null;
+        } else {
+            T item = list[nextFirst + 1];
+            list[nextFirst + 1] = null;
+            nextFirst++;
+            return item;
+        }
+    }
+
+    @Override
+    public T removeLast() {
+        size--;
+        if (nextLast == 0) {
+            return null;
+        } else {
+            T item = list[nextLast - 1];
+            list[nextLast - 1] = null;
+            nextLast--;
+            return item ;
+        }
+
+    }
+
+    @Override
+    public T get(int index) {
+        if (index >= size || index < 0) {
+            return null;
+        }
+        return list[index];
+    }
+}
+
+
---
+++ proj1b/tests/ArrayDequeTest.java
@@ -0,0 +1,402 @@
+import jh61b.utils.Reflection;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+import java.util.List;
+
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.Truth.assertWithMessage;
+
+public class ArrayDequeTest {
+
+
+
+    /** Performs some basic linked list tests. */
+
+
+        @Test
+        /** In this test, we have three different assert statements that verify that addFirst works correctly. */
+        public void addFirstTestBasic() {
+            Deque<String> lld1 = new ArrayDeque<> ();
+
+            lld1.addFirst("back"); // after this call we expect: ["back"]
+            assertThat(lld1.toList()).containsExactly("back").inOrder();
+
+            lld1.addFirst("middle"); // after this call we expect: ["middle", "back"]
+            assertThat(lld1.toList()).containsExactly("middle", "back").inOrder();
+
+            lld1.addFirst("front"); // after this call we expect: ["front", "middle", "back"]
+            assertThat(lld1.toList()).containsExactly("front", "middle", "back").inOrder();
+
+         /* Note: The first two assertThat statements aren't really necessary. For example, it's hard
+            to imagine a bug in your code that would lead to ["front"] and ["front", "middle"] failing,
+            but not ["front", "middle", "back"].
+          */
+        }
+
+        @Test
+        /** In this test, we use only one assertThat statement. IMO this test is just as good as addFirstTestBasic.
+         *  In other words, the tedious work of adding the extra assertThat statements isn't worth it. */
+        public void addLastTestBasic() {
+            Deque<String> lld1 = new  ArrayDeque<>();
+
+            lld1.addLast("front"); // after this call we expect: ["front"]
+            lld1.addLast("middle"); // after this call we expect: ["front", "middle"]
+            lld1.addLast("back"); // after this call we expect: ["front", "middle", "back"]
+            assertThat(lld1.toList()).containsExactly("front", "middle", "back").inOrder();
+        }
+
+        @Test
+        /** This test performs interspersed addFirst and addLast calls. */
+        public void addFirstAndAddLastTest() {
+            Deque<Integer> lld1 = new  ArrayDeque<>();
+
+         /* I've decided to add in comments the state after each call for the convenience of the
+            person reading this test. Some programmers might consider this excessively verbose. */
+            lld1.addLast(0);   // [0]
+            lld1.addLast(1);   // [0, 1]
+            lld1.addFirst(-1); // [-1, 0, 1]
+            lld1.addLast(2);   // [-1, 0, 1, 2]
+            lld1.addFirst(-2); // [-2, -1, 0, 1, 2]
+
+            assertThat(lld1.toList()).containsExactly(-2, -1, 0, 1, 2).inOrder();
+        }
+
+//     Below, you'll write your own tests for LinkedListDeque.
+
+        @Test
+        /** This test performs interspersed addFirst and addLast calls. */
+        public void getSize() {
+            Deque<Integer> lld1 = new  ArrayDeque<>();
+
+         /* I've decided to add in comments the state after each call for the convenience of the
+            person reading this test. Some programmers might consider this excessively verbose. */
+            lld1.addLast(0);   // [0]
+            lld1.addLast(1);   // [0, 1]
+            lld1.addFirst(-1); // [-1, 0, 1]
+            lld1.addLast(2);   // [-1, 0, 1, 2]
+            lld1.addFirst(-2); // [-2, -1, 0, 1, 2]
+
+
+            assertThat(lld1.size()).isEqualTo(5);
+        }
+        @Test
+        public void isLinkedListEmpty() {
+            Deque<Integer> lld1 = new  ArrayDeque<>();
+         /* I've decided to add in comments the state after each call for the convenience of the
+            person reading this test. Some programmers might consider this excessively verbose. */
+            lld1.addLast(0);   // [0]
+            lld1.addLast(1);   // [0, 1]
+            lld1.addFirst(-1); // [-1, 0, 1]
+            lld1.addLast(2);   // [-1, 0, 1, 2]
+            lld1.addFirst(-2); // [-2, -1, 0, 1, 2]
+
+            assertThat(lld1.isEmpty()).isEqualTo(false);
+        }
+
+        @Test
+        public void LinkedListIsEmpty() {
+            Deque<Integer> lld1 = new  ArrayDeque<>();
+         /* I've decided to add in comments the state after each call for the convenience of the
+            person reading this test. Some programmers might consider this excessively verbose. */
+
+            assertThat(lld1.isEmpty()).isEqualTo(true);
+        }
+
+        @Test
+        public void deleteLastTwo() {
+            Deque<Character> lld1 = new  ArrayDeque<>();
+         /* I've decided to add in comments the state after each call for the convenience of the
+            person reading this test. Some programmers might consider this excessively verbose. */
+            lld1.addLast('A');   // [A]
+            lld1.addLast('B');   // [A, B]
+            lld1.addLast('C'); // [A, B, C]
+            lld1.addLast('D');   // [A, B, C, D]
+            lld1.removeLast();
+            lld1.removeLast();
+
+
+            assertThat(lld1.removeLast()).isEqualTo('B');
+        }
+
+
+        @Test
+        public void getElement() {
+            Deque<Character> lld1 = new  ArrayDeque<>();
+         /* I've decided to add in comments the state after each call for the convenience of the
+            person reading this test. Some programmers might consider this excessively verbose. */
+            lld1.addLast('A');   // [A]
+            lld1.addLast('B');   // [A, B]
+            lld1.addLast('C'); // [A, B, C]
+            lld1.addLast('D');   // [A, B, C, D]
+            lld1.addLast('E');
+            lld1.addLast('F');
+
+
+            assertThat(lld1.get(3)).isEqualTo('D');
+        }
+
+        @Test
+        public void checkTillEmpty() {
+            Deque<Character> lld1 = new  ArrayDeque<>();
+         /* I've decided to add in comments the state after each call for the convenience of the
+            person reading this test. Some programmers might consider this excessively verbose. */
+            lld1.addLast('A');   // [A]
+            lld1.addLast('B');   // [A, B]
+            lld1.addLast('C'); // [A, B, C]
+            lld1.addLast('D');   // [A, B, C, D]
+            lld1.addLast('E');
+            lld1.addLast('F');
+
+            lld1.removeLast();
+            lld1.removeLast();
+            lld1.removeLast();
+            lld1.removeLast();
+            lld1.removeFirst();
+            lld1.removeFirst();
+
+
+
+            assertThat(lld1.isEmpty()).isEqualTo(true);
+        }
+
+        @Test
+        /** This test performs interspersed addFirst and addLast calls. */
+        public void addLastTest() {
+            Deque<String> lld1 = new  ArrayDeque<>();
+
+         /* I've decided to add in comments the state after each call for the convenience of the
+            person reading this test. Some programmers might consider this excessively verbose. */
+            lld1.addLast("Matt");
+            lld1.addLast("Alec");
+            lld1.addLast("Martina");
+            lld1.addLast("Sobec");
+            lld1.addLast("Nicolas");
+
+            assertThat(lld1.toList()).containsExactly("Matt", "Alec", "Martina", "Sobec", "Nicolas").inOrder();
+
+            lld1.removeLast();
+            lld1.addFirst("Kevin");
+            assertThat(lld1.toList()).containsExactly("Kevin","Matt", "Alec", "Martina", "Sobec").inOrder();
+        }
+
+        @Test
+        public void getElementNegative() {
+            Deque<Integer> lld1 = new  ArrayDeque<>();
+         /* I've decided to add in comments the state after each call for the convenience of the
+            person reading this test. Some programmers might consider this excessively verbose. */
+            lld1.addLast(-1);
+            lld1.addLast(-200);
+            lld1.addLast(10);
+            lld1.addLast(0);
+            lld1.addLast(78);
+            lld1.addLast(-9);
+
+
+            assertThat(lld1.get(-1)).isEqualTo(null);
+        }
+        @Test
+        public void getElement2() {
+            Deque<Character> lld1 = new  ArrayDeque<>();
+         /* I've decided to add in comments the state after each call for the convenience of the
+            person reading this test. Some programmers might consider this excessively verbose. */
+            lld1.addLast('A');   // [A]
+            lld1.addLast('B');   // [A, B]
+            lld1.addLast('C'); // [A, B, C]
+            lld1.addLast('D');   // [A, B, C, D]
+            lld1.addLast('E');
+            lld1.addLast('F');
+            lld1.addLast('Q');
+
+
+            assertThat(lld1.get(6)).isEqualTo('Q');
+        }
+
+        @Test
+        public void getElement5() {
+            Deque<Character> lld1 = new  ArrayDeque<>();
+         /* I've decided to add in comments the state after each call for the convenience of the
+            person reading this test. Some programmers might consider this excessively verbose. */
+            lld1.addLast('A');   // [A]
+            lld1.addLast('B');   // [A, B]
+            lld1.addLast('C'); // [A, B, C]
+            lld1.addLast('D');   // [A, B, C, D]
+            lld1.addLast('E');
+            lld1.addLast('F');
+
+            assertThat(lld1.get(0)).isEqualTo('A');
+            assertThat(lld1.get(1)).isEqualTo('B');
+            assertThat(lld1.get(2)).isEqualTo('C');
+            assertThat(lld1.get(3)).isEqualTo('D');
+            assertThat(lld1.get(4)).isEqualTo('E');
+            assertThat(lld1.get(5)).isEqualTo('F');
+
+        }
+        @Test
+        public void removeElemTest() {
+            Deque<Character> lld1 = new  ArrayDeque<>();
+         /* I've decided to add in comments the state after each call for the convenience of the
+            person reading this test. Some programmers might consider this excessively verbose. */
+            lld1.addLast('A');   // [A]
+            lld1.addFirst('B');   // [A, B]
+            lld1.addLast('C'); // [A, B, C]
+            lld1.addLast('D');   // [A, B, C, D]
+            lld1.addLast('E');
+            lld1.addLast('F');
+
+            lld1.removeLast();
+            lld1.removeLast();
+            lld1.removeLast();
+            lld1.removeLast();
+            lld1.removeLast();
+            lld1.removeLast();
+            lld1.addLast('K');
+
+
+
+            assertThat(lld1.isEmpty()).isEqualTo(false);
+        }
+
+        @Test
+        /** This test performs interspersed addFirst and addLast calls. */
+        public void getOutOfBoundsTest() {
+            Deque<String> lld = new  ArrayDeque<>();
+
+         /* I've decided to add in comments the state after each call for the convenience of the
+            person reading this test. Some programmers might consider this excessively verbose. */
+            lld.addFirst("Matt");
+            lld.addFirst("Alec");
+            lld.addFirst("Martina");
+            lld.addFirst("Sobec");
+            lld.addLast("Nicolas");
+
+            assertThat(lld.toList()).containsExactly("Sobec", "Martina", "Alec", "Matt", "Nicolas").inOrder();
+
+            lld.removeLast();
+            lld.addFirst("Kevin");
+            assertThat(lld.toList()).containsExactly("Kevin", "Sobec", "Martina", "Alec", "Matt").inOrder();
+        }
+
+        @Test
+        /** This test performs interspersed addFirst and addLast calls. */
+        public void toListCheck() {
+            Deque<String> lld = new  ArrayDeque<>();
+
+         /* I've decided to add in comments the state after each call for the convenience of the
+            person reading this test. Some programmers might consider this excessively verbose. */
+            lld.addFirst("Matt");
+            lld.addFirst("Alec");
+            lld.addFirst("Martina");
+            lld.addFirst("Sobec");
+            lld.addLast("Nicolas");
+
+            assertThat(lld.toList()).containsExactly ("Sobec", "Martina", "Alec", "Matt", "Nicolas").inOrder();
+
+        }
+
+
+
+
+        @Test
+        public void toListEmpty2() {
+            Deque<String> lld = new  ArrayDeque<>();
+
+         /* I've decided to add in comments the state after each call for the convenience of the
+            person reading this test. Some programmers might consider this excessively verbose. */
+
+
+            assertThat(lld.toList()).isEmpty();
+
+        }
+
+
+
+        @Test
+        public void removeFirstTest() {
+            Deque<String> lld = new  ArrayDeque<>();
+
+         /* I've decided to add in comments the state after each call for the convenience of the
+            person reading this test. Some programmers might consider this excessively verbose. */
+
+            lld.addFirst("Matt");
+            lld.removeFirst();
+
+            assertThat(lld.toList()).isEmpty();
+
+        }
+
+        @Test
+        public void removeFirstToEmptyTest() {
+            Deque<String> lld = new  ArrayDeque<>();
+
+         /* I've decided to add in comments the state after each call for the convenience of the
+            person reading this test. Some programmers might consider this excessively verbose. */
+
+            lld.addFirst("Matt");
+            lld.removeFirst();
+            assertThat(lld.toList ()).isEmpty();
+
+        }
+        @Test
+        public void removeLastToEmptyTest() {
+            Deque<String> lld5 = new  ArrayDeque<>();
+
+         /* I've decided to add in comments the state after each call for the convenience of the
+            person reading this test. Some programmers might consider this excessively verbose. */
+
+            lld5.addFirst("Matt");
+            lld5.removeFirst ();
+            assertThat(lld5.toList()).isEmpty();
+
+        }
+
+
+        @Test
+        public void removeFirstTillEmpty() {
+            Deque<String> lld5 = new  ArrayDeque<>();
+
+         /* I've decided to add in comments the state after each call for the convenience of the
+            person reading this test. Some programmers might consider this excessively verbose. */
+
+            lld5.addFirst("Matt");
+            lld5.addFirst("Matt");
+            lld5.addFirst("Matt");
+            lld5.addFirst("Matt");
+            lld5.addFirst("Matt");
+
+
+
+            lld5.removeFirst();
+            lld5.removeFirst();
+            lld5.removeFirst();
+            lld5.removeFirst();
+            lld5.removeFirst();
+
+
+            assertThat(lld5.toList()).isEmpty();
+
+        }
+
+        @Test
+        public void getOob_large() {
+            Deque<Character> lld1 = new  ArrayDeque<>();
+         /* I've decided to add in comments the state after each call for the convenience of the
+            person reading this test. Some programmers might consider this excessively verbose. */
+            lld1.addLast('A');   // [A]
+            lld1.addLast('B');   // [A, B]
+            lld1.addLast('C'); // [A, B, C]
+            lld1.addLast('D');   // [A, B, C, D]
+            lld1.addLast('E');
+            lld1.addLast('F');
+            lld1.addLast('Q');
+
+
+            assertThat(lld1.get(600)).isEqualTo(null);
+        }
+
+
+    }
+
+
+
